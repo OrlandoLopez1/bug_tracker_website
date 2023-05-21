0ac4b2052b1aa3c5d6af079119434fae39c77c0e
OrlandoLopez1 <orlando.lopez@ufl.edu>
2023-05-21 10:07:22 -0400

    applied bootstrap to table

--- client/src/components/TicketTable.js
+++ client/src/components/TicketTable.js
@@ -2,20 +2,21 @@ import React from 'react';
 
 function TicketTable({ tickets }) {
     return (
-        <table>
+        <table className="table">
             <thead>
             <tr>
-                <th>Title</th>
-                <th>Description</th>
-                <th>Assigned By</th>
-                <th>Assigned To</th>
-                <th>Status</th>
-                <th>Priority</th>
+                <th scope="col">Title</th>
+                <th scope="col">Description</th>
+                <th scope="col">Assigned By</th>
+                <th scope="col">Assigned To</th>
+                <th scope="col">Status</th>
+                <th scope="col">Priority</th>
             </tr>
             </thead>
             <tbody>
-            {tickets.map((ticket) => (
+            {tickets.map((ticket, index) => (
                 <tr key={ticket.title}>
+                    <th scope="row">{index + 1}</th>
                     <td>{ticket.title}</td>
                     <td>{ticket.description}</td>
                     <td>{ticket.assignedBy}</td>
