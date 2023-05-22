780bdc224778e9b8d169022e805c1ebadab3dbab
Orlando <orlando.lopez@ufl.edu>
2023-05-22 14:08:12 -0400

    added side menu and navbar to addTicket page

--- client/src/components/TicketForm.js
+++ client/src/components/TicketForm.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { createTicket } from '../controllers/TicketController'
 import CustomNavbar from "./CustomNavbar";
 import SideMenu from "./SideMenu";
+import TicketTable from "./TicketTable";
 
 function CreateTicketPage() {
     const [title, setTitle] = useState('');
@@ -13,6 +14,7 @@ function CreateTicketPage() {
     const [project_name, setProject] = useState('');
     const [status, setStatus] = useState('open');
     const [priority, setPriority] = useState('medium');
+    const [username, setUsername] = useState(null);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -32,85 +34,100 @@ function CreateTicketPage() {
         }
     };
 
+    useEffect(() => {
+        const usernameFromStorage = localStorage.getItem('username');
+
+        if (usernameFromStorage) {
+            setUsername(usernameFromStorage);
+        }
+    }, []);
+
     return (
         <Form onSubmit={handleSubmit}>
-            <Form.Group>
-                <Form.Label>Title</Form.Label>
-                <Form.Control
-                    type="text"
-                    value={title}
-                    onChange={e => setTitle(e.target.value)}
-                />
-            </Form.Group>
+            <CustomNavbar username={username} />
+            <div className="content-container">
+                <SideMenu />
+                <div className="ticket-page-content">
 
-            <Form.Group>
-                <Form.Label>Type</Form.Label>
-                <Form.Control
-                    type="text"
-                    value={type}
-                    onChange={e => setType(e.target.value)}
-                />
-            </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={title}
+                            onChange={e => setTitle(e.target.value)}
+                        />
+                    </Form.Group>
 
-            <Form.Group>
-                <Form.Label>Project</Form.Label>
-                <Form.Control
-                    type="text"
-                    value={project_name}
-                    onChange={e => setProject(e.target.value)}
-                />
-            </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Type</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={type}
+                            onChange={e => setType(e.target.value)}
+                        />
+                    </Form.Group>
 
-            <Form.Group>
-                <Form.Label>Description</Form.Label>
-                <Form.Control
-                    as="textarea"
-                    rows={3}
-                    value={description}
-                    onChange={e => setDescription(e.target.value)}
-                />
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Assigned By</Form.Label>
-                <Form.Control
-                    type="text"
-                    value={assignedBy}
-                    onChange={e => setAssignedBy(e.target.value)}
-                />
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Assigned To</Form.Label>
-                <Form.Control
-                    type="text"
-                    value={assignedTo}
-                    onChange={e => setAssignedTo(e.target.value)}
-                />
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Status</Form.Label>
-                <Form.Control
-                    as="select"
-                    value={status}
-                    onChange={e => setStatus(e.target.value)}
-                >
-                    <option value="open">Open</option>
-                    <option value="in-progress">In Progress</option>
-                    <option value="closed">Closed</option>
-                </Form.Control>
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Priority</Form.Label>
-                <Form.Control
-                    as="select"
-                    value={priority}
-                    onChange={e => setPriority(e.target.value)}
-                >
-                    <option value="low">Low</option>
-                    <option value="medium">Medium</option>
-                    <option value="high">High</option>
-                </Form.Control>
-            </Form.Group>
-            <Button variant="primary" type="submit">Submit</Button>
+                    <Form.Group>
+                        <Form.Label>Project</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={project_name}
+                            onChange={e => setProject(e.target.value)}
+                        />
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control
+                            as="textarea"
+                            rows={3}
+                            value={description}
+                            onChange={e => setDescription(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Assigned By</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={assignedBy}
+                            onChange={e => setAssignedBy(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Assigned To</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={assignedTo}
+                            onChange={e => setAssignedTo(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Status</Form.Label>
+                        <Form.Control
+                            as="select"
+                            value={status}
+                            onChange={e => setStatus(e.target.value)}
+                        >
+                            <option value="open">Open</option>
+                            <option value="in-progress">In Progress</option>
+                            <option value="closed">Closed</option>
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Priority</Form.Label>
+                        <Form.Control
+                            as="select"
+                            value={priority}
+                            onChange={e => setPriority(e.target.value)}
+                        >
+                            <option value="low">Low</option>
+                            <option value="medium">Medium</option>
+                            <option value="high">High</option>
+                        </Form.Control>
+                    </Form.Group>
+                    <Button variant="primary" type="submit">Submit</Button>
+                    </div>
+            </div>
         </Form>
     );
 }
