697396853d4ad041c53d1bc4b5bf242f3cd56281
OrlandoLopez1 <orlando.lopez@ufl.edu>
2023-05-23 14:00:11 -0400

    updates schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     profilePicture: { type: String, default: '/defaultpfp.png' },
+    role: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('User', userSchema);
