d4db94958d3b511ec6c3f1e6964eb4cb50c14418
Orlando <orlando.lopez@ufl.edu>
2023-06-05 10:20:41 -0400

    makes the username a dropdown and gives the user an option to logout

--- client/src/components/CustomNavbar.js
+++ client/src/components/CustomNavbar.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar } from 'react-bootstrap';
+import {Nav, Navbar, Dropdown} from 'react-bootstrap';
 import "./CustomNavbar.css";
 import jwtDecode from "jwt-decode";
 
@@ -18,16 +18,29 @@ function CustomNavbar() {
 
     return (
         <Navbar bg="dark" variant="dark" className="custom-navbar">
-            <Navbar.Brand href="#home">
-                Placeholder
-            </Navbar.Brand>
+            <Navbar.Brand href="#home">Placeholder</Navbar.Brand>
             <Navbar.Collapse className="justify-content-end">
                 <Navbar.Text>
-                    Signed in as: <a href="#login">{userName}</a>
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                        <Dropdown className="custom-dropdown">
+                            <Dropdown.Toggle variant="transparent" id="dropdown-basic" className="custom-dropdown-toggle">
+                                <img src={"/defaultpfp.jpg"} alt="user" className="profile-pic"/>
+                                <span style={{ marginRight: '10px' }}>{userName}</span>
+                            </Dropdown.Toggle>
+
+                            <Dropdown.Menu className="dropdown-menu-dark">
+                                <Dropdown.Item href="#action/1" className="no-hover">Profile</Dropdown.Item>
+                                <Dropdown.Item href="#action/2" className="no-hover">Settings</Dropdown.Item>
+                                <Dropdown.Item href="#action/3" className="no-hover">Logout</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
                 </Navbar.Text>
             </Navbar.Collapse>
         </Navbar>
     );
+
+
 }
 
 export default CustomNavbar;
